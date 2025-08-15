c797dec6b51c553f022986893819fdc45f127e26
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-08-15 22:02:49 +0530

    js

---
+++ 03_JS_Basics/else.js
@@ -0,0 +1,13 @@
+let size = "L";
+if(size === "XL"){
+    console.log("Price = 250");
+}
+else if(size === "L"){
+    console.log("Price = 200");
+}
+else if(size === "M"){
+    console.log("Price = 100");
+}
+else{
+    console.log("Price = 50");
+}
