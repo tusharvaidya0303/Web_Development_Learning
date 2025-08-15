b06e450b2487cb042a53cdb138b66f97d0c159f3
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-08-15 21:44:50 +0530

    js

---
+++ 03_JS_Basics/if-ststement.js
@@ -0,0 +1,11 @@
+let color = "red";
+
+if (color == "red") {
+    console.log("Stop. The color is red");
+}
+if (color == "yellow") {
+    console.log("Ready. The color is yellow");
+}
+if (color == "green") {
+    console.log("Go. The color is green");
+}
