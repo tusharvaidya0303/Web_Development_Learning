569e42346980de22cacdf351ffe0a6cf87625608
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-08-15 22:02:29 +0530

    js

---
+++ 03_JS_Basics/else-if.js
@@ -0,0 +1,13 @@
+let marks = 60;
+if(marks >= 90){
+    console.log("A+. Excillent");
+}
+else if(marks >= 60){
+    console.log("B. Good");
+}
+else if(marks >= 33){
+    console.log("C. Average");
+}
+else if(marks < 33){
+    console.log("F. Fail");
+}
