80da6eec97edd2b0de58e72cee5a5dd7fdbf29c9
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-08-17 14:59:09 +0530

    js

---
+++ 03_JS_Basics/switchCase.js
@@ -0,0 +1,14 @@
+let color = "red";
+switch(color){
+    case "red":
+    console.log("Stop");
+    break;
+    case "yellow":
+    console.log("Ready");
+    break;
+    case "green":
+    console.log("Go");
+    break;
+    default:
+    console.log("Light is Broken");
+}
