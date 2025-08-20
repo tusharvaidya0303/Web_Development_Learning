07cc16d83559f5ee4bf7c1de2695a8be6f27f782
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-08-20 20:18:23 +0530

    js

---
+++ 03_JS_Basics/practice2.js
@@ -0,0 +1,3 @@
+// Qs1.Write a JavaScript program to get the first n elements of an array.
+// [n can be any positive number]. Forexample: for array[7,9,0,-2] and n=3 Print,[7,9,0]
+let arr = [7,9,0,-2]
