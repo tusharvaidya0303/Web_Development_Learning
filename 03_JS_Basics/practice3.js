74a53fc463e5a5488a732936c36839d0cdae39ab
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-08-23 15:21:51 +0530

    js

--- 03_JS_Basics/practice3.js
+++ 03_JS_Basics/practice3.js
@@ -1,15 +1,17 @@
 // Qs1.Write a JS program to delete all occurrences of element ‘num’ in a given array.
 // Example: if arr=[1,2,3,4,5,6,2,3] & num = 2 Result should be arr =[1,3,4,5,6,3]
-let arr = [1, 2, 3, 4, 5, 6, 2, 3];
-let num = 2;
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] == 2) {
-    arr.splice(i, 1);
-  }
-}
-console.log(arr);
+// let arr = [1, 2, 3, 4, 5, 6, 2, 3];
+// let num = 2;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] == 2) {
+//     arr.splice(i, 1);
+//   }
+// }
+// console.log(arr);
+
 
 // Qs2.Write a JS program to find the no of digits in a number. Example: if number = 287152, count=6.
+
 
 // Qs3.Write a JS program to find the sum of digits in a number. Example: if number=287152, sum=25.
 
