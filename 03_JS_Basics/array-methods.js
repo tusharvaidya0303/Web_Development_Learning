0f76098954d4a591aa7df539f3afd9eae6bd995a
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-09-05 10:58:28 +0530

    forEach

---
+++ 03_JS_Basics/array-methods.js
@@ -0,0 +1,16 @@
+// Array Methods
+// 1) ForEach
+
+let arr = [1,2,3,4,5]
+// let print = function(el){   //using normal function
+//     console.log(el);
+// }
+// arr.forEach(print)
+
+// arr.forEach((call)=>{      //using arrow function
+//     console.log(call);
+// })
+
+//forEach with object
+
+
