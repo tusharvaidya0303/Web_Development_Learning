1d01638c919e6b7d8bcaef544b273e5b01dade9d
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-09-15 20:29:04 +0530

    practice

--- 05_DOM/Practic/script.js
+++ 05_DOM/Practic/script.js
@@ -18,7 +18,7 @@ h1.innerHTML="<u>DOM Practice</u>"
 document.querySelector("body").prepend(h1);
 
 let p = document.createElement("p");
-h1.innerHTML="<u>DOM Practice</u>"
+h1.innerHTML="<u>Delta</u>"
 
 document.querySelector("body").prepend(h1);
 
