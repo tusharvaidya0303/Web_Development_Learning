bfff3140cca3cdb291e704d56fc651660a0c2aa4
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-09-15 19:56:32 +0530

    Practice

---
+++ 05_DOM/Practic/script.js
@@ -0,0 +1,11 @@
+let button = document.createElement("button");
+let input = document.createElement("input");
+
+button.innerText="Click me !!"
+
+document.querySelector("body").append(input);
+document.querySelector("body").append(button);
+
+button.setAttribute("id","btn");
+input.setAttribute("placeholder","Username")
+
