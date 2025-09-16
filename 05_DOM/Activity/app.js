dd7736df702e4f4d941792620c21ac6bf1646719
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-09-16 15:03:41 +0530

    Activity

---
+++ 05_DOM/Activity/app.js
@@ -0,0 +1,8 @@
+function getRandomColor(){
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    let color = `rgb(${red},${green},${blue})`;
+    return color;
+}
