15ec4764abcf2c5d4ee964b5dadd851027a3423e
Tushar Vaidya <tusharvaidya0303@gmail.com>
2025-09-29 18:10:38 +0530

    Add math utility functions for sum, multiply, and pi

---
+++ 06_Backend/math.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+const pi = 3.14159;
+
+let obj = {
+    sum: sum,
+    multiply: multiply,
+    pi: pi
+}
+module.exports = obj;
+// Compare this snippet from 06_Backend/math.js:
