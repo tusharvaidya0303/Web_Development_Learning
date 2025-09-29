dd56c7f9448a28bf1207f20442a642937b94d2c6
Tushar Vaidya <tusharvaidya0303@gmail.com>
2025-09-29 18:45:39 +0530

    Add fruit modules for Apple, Mango, and Orange; update script to log fruit information

---
+++ 06_Backend/Fruits/orange.js
@@ -0,0 +1,5 @@
+module.exports = {   
+    name: "Orange",
+    color: "orange",
+    taste: "citrus",
+}
--- 06_Backend/script.js
+++ 06_Backend/script.js
@@ -12,8 +12,13 @@
 //     console.log(`Hello ${i}`);
 // }
 
-const math = require('./math');
+// const math = require('./math');
+// console.log(math.sum(5,3));
+// console.log(math.multiply(5,3));
+// console.log(math.pi);
 
-console.log(math.sum(5,3));
-console.log(math.multiply(5,3));
-console.log(math.pi);
+const info = require('./Fruits');
+console.log(info);
+// console.log(info[0]);
+// console.log(info[1]);
+// console.log(info[2]);
