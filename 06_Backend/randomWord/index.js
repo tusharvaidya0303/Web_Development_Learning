b33374b2833320850c6e5173bedcbc666772461b
Tushar Vaidya <tusharvaidya0303@gmail.com>
2025-10-08 17:19:09 +0530

    Add initial project files including math functions, random word generation, and package configuration

---
+++ 06_Backend/randomWord/index.js
@@ -0,0 +1,8 @@
+import { add, pi } from "./math.js";
+console.log("2 + 3 =", add(2, 3));
+console.log("Value of pi:", pi);
+
+// To run this file, use the command: node index.js
+
+import { generate } from "random-words";
+console.log(`random word: ${generate()}`);
---
+++ 06_Backend/randomWord/math.js
@@ -0,0 +1,4 @@
+export const add = (a, b) => {
+  return a + b;
+};
+export const pi = 3.14;
