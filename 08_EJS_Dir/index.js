039c0c2e3c68c1533330fbd9b74fed880e1e674a
Tushar Vaidya <tusharvaidya0303@gmail.com>
2025-10-24 16:21:03 +0530

    EJS

--- 08_EJS_Dir/index.js
+++ 08_EJS_Dir/index.js
@@ -3,6 +3,12 @@ const app = express();
 
 const port = 8080;
 
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('this is home');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
