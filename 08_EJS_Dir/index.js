dffe65d6128f05b2781251768942ecd465e91a9f
Tushar Vaidya <tusharvaidya0303@gmail.com>
2025-10-25 14:46:29 +0530

    EJS

--- 08_EJS_Dir/index.js
+++ 08_EJS_Dir/index.js
@@ -19,10 +19,19 @@ app.get('/rolldice', (req, res) => {
     res.render('rolldice.ejs', { diceVal });
 });
 
-app.get('/instagram/:username', (req, res) => {
-    const followers = ["Adam", "Eve", "Charlie", "Dave", "Mallory"];
+// app.get('/instagram/:username', (req, res) => {
+//     const followers = ["Adam", "Eve", "Charlie", "Dave", "Mallory"];
+//     const { username } = req.params;
+//     res.render('instagram.ejs', { username, followers });
+// });
+
+app.get('/ig/:username', (req, res) => {
     const { username } = req.params;
-    res.render('instagram.ejs', { username, followers });
+    const instaData = require('./data.json');
+    const data = instaData[username];
+    console.log(data);
+    
+    res.render('instagram.ejs', { data });
 });
 
 app.listen(port, () => {
