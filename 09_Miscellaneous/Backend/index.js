ccfce3220229efb726d04ee64b2fedfa50f7b9c8
Tushar Vaidya <tusharvaidya0303@gmail.com>
2025-10-26 18:21:17 +0530

    GET & POST request

--- 09_Miscellaneous/Backend/index.js
+++ 09_Miscellaneous/Backend/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use (express.urlencoded({ extended: true }));
+app.use (express.json());
+
 const port = 8080;
 
 app.get('/register', (req, res) => {
-  res.send('GET request to the register');
+    const { username } = req.query;
+  res.send(`GET request to Login. Welcome ${username}`);
 });
 app.post('/register', (req, res) => {
-  res.send('POST request to the register');
+  const { username } = req.body;
+  res.send(`POST request to Login. Welcome ${username}`);
 });
 
 app.listen(port, () => {
