d1326ee4cdd4b7da0f7678c8e4c17bbad6d2bc06
Tushar Vaidya <tusharvaidya0303@gmail.com>
2025-10-27 12:11:54 +0530

    REST APIs

--- 10_REST_CLASS/index.js
+++ 10_REST_CLASS/index.js
@@ -1,20 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
-const path = require('path');
+const path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Home Page');
+let posts = [
+  {
+    username: "Tushar Vaidya",
+    content: "This is the content of the first post.",
+  },
+  {
+    username: "Ketan Vishwakarma",
+    content: "Hello world!",
+  },
+  {
+    username: "Aman Vishwakarma",
+    content: "Learning webdev.",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
 });
 
-app.listen(port, () =>{
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
