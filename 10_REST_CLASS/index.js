57a12b54d11b8178c35b8171fddf9694bd9349c4
Tushar Vaidya <tusharvaidya0303@gmail.com>
2025-10-27 11:36:43 +0530

    REST APIs

--- 10_REST_CLASS/index.js
+++ 10_REST_CLASS/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const path = require('path');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Home Page');
+});
 
 app.listen(port, () =>{
     console.log(`Server is running on http://localhost:${port}`);
