3b259630b41c90370503a92629028e3da8523beb
Tushar Vaidya <tusharvaidya0303@gmail.com>
2025-10-31 16:14:08 +0530

    mysql 2 package

--- 11_SQLCLASS/index.js
+++ 11_SQLCLASS/index.js
@@ -1,12 +1,22 @@
 // CJS
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
+// Get the client
+const mysql = require("mysql2");
 
-let getRandomUser= () => {  
+// Create the connection to database
+const connection = await mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "test",
+  password: "Tushar@123",
+});
+
+let getRandomUser = () => {
   return {
     Id: faker.string.uuid(),
     username: faker.internet.username(),
     email: faker.internet.email(),
     password: faker.internet.password(),
   };
-}
+};
 console.log(getRandomUser());
