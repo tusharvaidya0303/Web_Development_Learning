f38130938f9b8320892fe26461125d84bb00e407
Tushar Vaidya <tusharvaidya0303@gmail.com>
2025-10-31 16:08:53 +0530

    SQL with node

--- 11_SQLCLASS/index.js
+++ 11_SQLCLASS/index.js
@@ -1,17 +1,12 @@
-
-
 // CJS
 const { faker } = require('@faker-js/faker');
 
-let createRandomUser= () => {  
+let getRandomUser= () => {  
   return {
-    userId: faker.string.uuid(),
+    Id: faker.string.uuid(),
     username: faker.internet.username(),
     email: faker.internet.email(),
-    avatar: faker.image.avatar(),
     password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
   };
 }
-console.log(createRandomUser());
+console.log(getRandomUser());
