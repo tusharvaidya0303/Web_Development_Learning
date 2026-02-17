35d6ad2316d8bd1d531476f15f38084ad03011eb
Tushar Vaidya <tusharvaidya0303@gmail.com>
2026-02-17 14:06:23 +0530

    Objects

--- 03_JavaScript/02_Basics/04_objects.js
+++ 03_JavaScript/02_Basics/04_objects.js
@@ -32,3 +32,19 @@ const regularUser = {
 console.log(regularUser["Full Name"]);
 console.log(regularUser["Full Name"].userFullName.firstName);
 
+const obj1 = {1: "one", 2: "two"};
+const obj2 = {3: "three", 4: "four"};
+const obj3 = {4: "four", 5: "five"};
+
+const mergedObj = {obj1, obj2, obj3}; // Merging objects using object literal syntax
+console.log(mergedObj); // Output: { obj1: { '1': 'one', '2': 'two' }, obj2: { '3': 'three', '4': 'four' }, obj3: { '4': 'four', '5': 'five' } }
+// In this example, we have three objects (obj1, obj2, and obj3) with some overlapping keys.
+
+const mergedObjassign = Object.assign({}, obj1, obj2, obj3); // Merging objects using Object.assign()
+console.log(mergedObjassign); // Output: { '1': 'one', '2': 'two', '3': 'three', '4': 'four', '5': 'five' }
+// The Object.assign() method is used to copy the values of all enumerable own properties from one or more
+// source objects to a target object.
+
+const mergedObjSpread = {...obj1, ...obj2, ...obj3}; // Merging objects using the spread operator
+console.log(mergedObjSpread); // Output: { '1': 'one', '2': 'two', '3': 'three', '4': 'four', '5': 'five' }
+// The spread operator (...) is a more concise and modern way to merge objects.
