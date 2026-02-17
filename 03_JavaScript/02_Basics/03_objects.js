8f3439a3d786412bc2baa2de15f2139b40b133b2
Tushar Vaidya <tusharvaidya0303@gmail.com>
2026-02-17 13:18:49 +0530

    Objects

--- 03_JavaScript/02_Basics/03_objects.js
+++ 03_JavaScript/02_Basics/03_objects.js
@@ -8,6 +8,7 @@ let symbol = Symbol("id"); // Creating a unique symbol
 
 const User = {
     name: "Tushar",
+    'Full Name': "Tushar Vaidya", // Property name with space, must be accessed using bracket notation
     age: 25,
     email: "tushar@example.com",
     isAdmin: true,
@@ -17,6 +18,8 @@ const User = {
 //Accessing object properties
 console.log(User.name); // Output: Tushar
 console.log(User['age']); // Output: 25
+console.log(User['Full Name']); // Output: Tushar Vaidya
+
 console.log(User[symbol]); // Output: unique_id
 // We can access object properties using dot notation (User.name) or bracket notation (User['age']).
 // For symbol properties, we must use bracket notation with the symbol variable (User[symbol]).
