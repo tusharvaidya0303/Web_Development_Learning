92d632093a62e80553b5a75253b6351c82cf5a4c
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-03-19 10:38:42 +0530

    arrowfunction

--- 03_JavaScript/03_Basics/03_arrowfunction.js
+++ 03_JavaScript/03_Basics/03_arrowfunction.js
@@ -8,6 +8,34 @@ const user = {
     }
 }
 
-user.welcomeMessage();
-user.name="Vaidya";
-user.welcomeMessage();
+// user.welcomeMessage();
+// user.name="Vaidya";
+// user.welcomeMessage();
+// console.log(this);    //this keyword refers to the global object. it willbe empty object.
+
+function one () {
+    let username = "Tushar"
+    console.log(this.username);  //Undefined because this keyword refers to the global object and global object has no username property
+    console.log(username); //Tushar username is accessible here
+}
+// one();
+
+//Arrow function
+const two = () => {
+    let username = "Tushar"
+    console.log(this.username);  //Undefined because this keyword refers to the global object and global object has no username property
+    console.log(username); //Tushar username is accessible here
+}
+// two();
+
+const addTwo = (num1, num2) => {
+    return num1 + num2;               //explicit return
+}
+// console.log(addTwo(2,3)); //output : 5
+
+// const addThree = (num1, num2) => num1 + num2;  //both are same
+const addThree = (num1, num2) => (num1 + num2);    //implicit return
+console.log(addThree(2,3));  //output : 5
+
+const obj =() => ({name:"Tushar", age:25})         //for object use curly braces inside parenthesis
+console.log(obj());
