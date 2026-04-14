2f68b5fbc804605d912541d7ec37ab8684689a05
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-04-14 17:32:59 +0530

    Rendering Arrays in React

--- 13_React/basic_react_app/src/Product.jsx
+++ 13_React/basic_react_app/src/Product.jsx
@@ -1,10 +1,11 @@
 import "./Product.css";
-function Product({title, price = 1}) {
+function Product({title, price, features}) {
   //price = 1 becomes default price if not passed
   return (
     <div className="Product">
       <h2>{title}</h2>
       <p>price : {price}</p>
+      <p>{features}</p>
     </div>
   );
 }
--- 13_React/basic_react_app/src/ProductTab.jsx
+++ 13_React/basic_react_app/src/ProductTab.jsx
@@ -1,13 +1,17 @@
 import Product from "./Product.jsx";
 
 function ProductTab() {
+    let option = ["high-tech","Durable","Fast"]
+    //Using objects
+    // let option2 = {a:"smooth",b:"long-lasting"}
+    //<Product title="pen" price={10}  features={{a:"smooth",b:"long-lasting"}}/>         
   return (
     //passing props to a component
     // {} is used to pass props which is not string
     <div>
-      <Product title="Laptop" price={10000} />
-      <Product title="Mobile" price={5000} />
-      <Product title="pen" />
+      <Product title="Laptop" price={10000} features={option} />
+      <Product title="Mobile" price={5000} features={option} />
+      <Product title="pen" price={10}  features={["smooth","long-lasting"]}/>  
     </div>
   );
 }
