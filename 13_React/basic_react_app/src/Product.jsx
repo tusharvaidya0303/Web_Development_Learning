4c95afe4196fab8b1e2b55530cf666de1a23302e
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-04-14 17:35:32 +0530

    Rendering Arrays in React

--- 13_React/basic_react_app/src/Product.jsx
+++ 13_React/basic_react_app/src/Product.jsx
@@ -1,11 +1,12 @@
 import "./Product.css";
 function Product({title, price, features}) {
   //price = 1 becomes default price if not passed
+  // let list = features.map((feature) => <li>{feature}</li>);
   return (
     <div className="Product">
       <h2>{title}</h2>
       <p>price : {price}</p>
-      <p>{features}</p>
+      <p>{features.map((feature) => <li>{feature}</li>)}</p>
     </div>
   );
 }
