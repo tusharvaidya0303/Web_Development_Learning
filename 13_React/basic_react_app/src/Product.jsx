306911f2f740ff122fe42490396d43437030cb60
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-04-14 21:06:33 +0530

    Dynamic Styling in React

--- 13_React/basic_react_app/src/Product.jsx
+++ 13_React/basic_react_app/src/Product.jsx
@@ -2,12 +2,14 @@ import "./Product.css";
 function Product({title, price, features}) {
   //price = 1 becomes default price if not passed
   // let list = features.map((feature) => <li>{feature}</li>);
+  let isDiscount = price >= 10000;
+  let styles = { backgroundColor: isDiscount ?"orange" : "pink" };
   return (
-    <div className="Product">
+    <div className="Product" style={styles}>
       <h2>{title}</h2>
       <p>price : {price}</p>
       <p>{features.map((feature) => <li>{feature}</li>)}</p>
-      {price >=10000 && <p>5% discount</p>}
+      {isDiscount && <p>5% discount</p>}
       {/* //{price >=10000 ? <p>5% discount</p> : null} */}
     </div>
   );
