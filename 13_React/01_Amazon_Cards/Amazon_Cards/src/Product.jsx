e850fa3887fde85370d69198556eecbf0dbd6128
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-04-15 17:59:23 +0530

    Amazon Cards practice

--- 13_React/01_Amazon_Cards/Amazon_Cards/src/Product.jsx
+++ 13_React/01_Amazon_Cards/Amazon_Cards/src/Product.jsx
@@ -2,24 +2,26 @@ import "./Product.css";
 import Prices from "./Prices";
 
 function Product({ title, idx }) {
-    let oldPrice = ["12,495","11,900","1,599","599"];
-    let newPrice = ["8,999","9,199","899","278"];
-    let description = ["8,000 Dpi","made for iPad pro","intutive surface","Wireless"];
+  let oldPrice = ["12,495", "11,900", "1,599", "599"];
+  let newPrice = ["8,999", "9,199", "899", "278"];
+  let description = [
+    ["8,000 Dpi", "5 Programmable Buttons"],
+    ["made for iPad pro","intutive surface"],
+    ["intutive surface","made for iPad pro"],
+    ["Wireless","Optical orientation"]
+  ];
 
-    return (
-        <div className="Product">
-                <h4>{title}</h4>
-                <p>{description[idx]}</p>
+  return (
+    <div className="Product">
+      <h4>{title}</h4>
+      <p>{description[idx][0]}</p>
+      <p>{description[idx][1]}</p>
 
-            <div className="product-price">
-                <Prices 
-                    oldPrice={oldPrice[idx]} 
-                    newPrice={newPrice[idx]} 
-                />
-            </div>
-
-        </div>
-    );
+      <div className="product-price">
+        <Prices oldPrice={oldPrice[idx]} newPrice={newPrice[idx]} />
+      </div>
+    </div>
+  );
 }
 
 export default Product;
