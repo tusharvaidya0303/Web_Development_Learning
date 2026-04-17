7645d63c136b1eca72313f3d9b4f161e8b608f2b
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-04-17 10:55:38 +0530

    ludoBoard Game

--- 13_React/03_Ludo_Board/src/LudoBoard.jsx
+++ 13_React/03_Ludo_Board/src/LudoBoard.jsx
@@ -1,17 +1,43 @@
-export default function ludoBoard(){
-    return(
-        <>
-        <p>Game Begins!</p>
-        <div className="board">
-            <p>Blue Moves = </p>
-            <button>+1</button>
-            <p>Yellow Moves = </p>
-            <button>+1</button>
-            <p>Green Moves = </p>
-            <button>+1</button>
-            <p>Red Moves = </p>
-            <button>+1</button>
-        </div>
-        </>
-    )
+import { useState } from "react";
+export default function LudoBoard() {
+  let [moves, setMoves] = useState({ blue: 0, yellow: 0, green: 0, red: 0 });
+  let updateBlue = () => {
+    console.log(`moves.blue = ${moves.blue}`);
+    setMoves((prevMoves)=>{
+      return { ...prevMoves, blue: prevMoves.blue + 1 };
+    });
+  }
+  let updateYellow = () => {
+    console.log(`moves.yellow = ${moves.yellow}`);
+    setMoves((prevMoves)=>{
+      return { ...prevMoves, yellow: prevMoves.yellow + 1 };
+    });
+  }
+  let updateGreen = () => {
+    console.log(`moves.green = ${moves.green}`);
+      setMoves((prevMoves)=>{
+      return { ...prevMoves, green: prevMoves.green + 1 };
+    });
+  }
+  let updateRed = () => {
+    console.log(`moves.red = ${moves.red}`);
+      setMoves((prevMoves)=>{
+      return { ...prevMoves, red: prevMoves.red + 1 };
+    });
+  }
+  return (
+    <>
+      <p>Game Begins!</p>
+      <div className="board">
+        <p>Blue Moves = {moves.blue}</p>
+        <button style={{backgroundColor : "blue"}} onClick={updateBlue}>+1</button>
+        <p>Yellow Moves = {moves.yellow}</p>
+        <button style={{backgroundColor : "Yellow", color : "black"}} onClick={updateYellow}>+1</button>
+        <p>Green Moves = {moves.green}</p>
+        <button style={{backgroundColor : "Green"} }onClick={updateGreen}>+1</button>
+        <p>Red Moves = {moves.red}</p>
+        <button style={{backgroundColor : "Red"}} onClick={updateRed}>+1</button>
+      </div>
+    </>
+  );
 }
