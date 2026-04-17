46fccf08377a913cac5a264138f469365ad64777
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-04-17 16:20:30 +0530

    ToDo list using react

--- 13_React/04_ToDo_List/src/TodoList.jsx
+++ 13_React/04_ToDo_List/src/TodoList.jsx
@@ -2,12 +2,12 @@ import "./TodoList.css"
 import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 export default function TodoList() {
-    let [todos, setTodos] = useState([{task:"Sample Task",id:uuidv4() }]);
+    let [todos, setTodos] = useState([{task:"Sample Task",id:uuidv4(), isDone:false }]);
     let [newtodos, setNewTodos] = useState("");
 
     function addNewTask() {
         setTodos( (privTodos)=> {
-            return [...privTodos,{task:newtodos,id:uuidv4()}];
+            return [...privTodos,{task:newtodos,id:uuidv4(), isDone:false }];
         });
         setNewTodos("");
     }
@@ -20,7 +20,16 @@ export default function TodoList() {
         });
         
     }
-    
+let markAsDone = (id) => {
+    setTodos(prevTodos =>
+        prevTodos.map(todo =>
+            todo.id === id ? { ...todo, isDone: true } : todo
+        )
+    );
+};
+let clearAll = () => {
+    setTodos([]);
+};
 
   return (
     <div className="parent">
@@ -30,14 +39,15 @@ export default function TodoList() {
       <h4>Task To Do</h4>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>
+          <li key={todo.id} className={todo.isDone ? "done" : ""}>
             <span>{todo.task}</span>
             &nbsp;&nbsp;&nbsp;&nbsp;
             <button className="remove-btn" onClick={() => deleteTask(todo.id)}>Delete</button>
-            <button className="done-btn" >Mark as Done</button>
+            <button className="done-btn" onClick={()=>{markAsDone(todo.id)}} >Mark as Done</button>
             </li>
         ))}
       </ul>
+      <button className="clear-btn" onClick={clearAll}>Clear All</button>
     </div>
   );
 }
