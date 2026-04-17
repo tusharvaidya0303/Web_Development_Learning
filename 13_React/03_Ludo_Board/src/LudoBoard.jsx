6194c4524ed743ae3c4efff2b0523fb60a1fdd7a
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-04-17 10:06:14 +0530

    ludoBoard Game

---
+++ 13_React/03_Ludo_Board/src/LudoBoard.jsx
@@ -0,0 +1,17 @@
+export default function ludoBoard(){
+    return(
+        <>
+        <p>Game Begins!</p>
+        <div className="board">
+            <p>Blue Moves = </p>
+            <button>+1</button>
+            <p>Yellow Moves = </p>
+            <button>+1</button>
+            <p>Green Moves = </p>
+            <button>+1</button>
+            <p>Red Moves = </p>
+            <button>+1</button>
+        </div>
+        </>
+    )
+}
