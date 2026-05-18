38e11f9ae4ea5f91e8280bccd66c13ba8adf6a25
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-05-18 13:58:45 +0530

    mongodb

---
+++ 12_MongoDB/index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+main().then(() => console.log('connected successfully'))
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
