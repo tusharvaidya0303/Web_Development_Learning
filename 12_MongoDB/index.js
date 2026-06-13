7c035ca1c473d96969615328c3f61865624d399a
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-06-13 12:42:33 +0530

    Remove node_modules

--- 12_MongoDB/index.js
+++ 12_MongoDB/index.js
@@ -1,8 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-main().then(() => console.log('connected successfully'))
-.catch(err => console.log(err));
+main()
+  .then(() => console.log("connected successfully"))
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
---
+++ 13_React/08_Job_Posts/Job_Posts/src/Components/Cards.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Users from "./Users";
+
+const Cards = () => {
+  return (
+    <>
+      <div ClassName="header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaL-7RRx5SW9qx4AJw4mjQHfar35O2ScabD2zkXUV8kg&s=10" alt="img" />
+        <button>Save</button>
+      </div>
+      <div className="middle"></div>
+      <div className="footer"></div>
+    </>
+  );
+};
+
+export default Cards;
